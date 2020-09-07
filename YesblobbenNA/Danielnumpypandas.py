4c047e6a629f9f2ded7567a402ca0bc617ec5c3c
YesblobbenNA <70510729+YesblobbenNA@users.noreply.github.com>
2020-09-07 16:05:44 +0200

    Daniel

---
+++ YesblobbenNA/Danielnumpypandas.py
@@ -0,0 +1,35 @@
+import numpy as np
+import pandas as pd
+
+number_array = []
+number_array2 = []
+number_array3 = []
+#Tomma listor
+
+count = 1
+#Startvärde
+
+while(count < 13):
+    if count < 5:
+        user_input = int(input(f'Skriv in lottonummer {count} på rad 1: '))
+        number_array.append(user_input)
+        pass
+    elif count < 9:
+        user_input = int(input(f'Skriv in lottonummer {count} på rad 2: '))
+        number_array2.append(user_input)
+        pass
+    else:
+        user_input = int(input(f'Skriv in lottonummer {count} på rad 3: '))
+        number_array3.append(user_input)
+    count = count + 1
+#Input från användare adderas till listorna
+
+number_array = np.array(number_array)
+number_array2 = np.array(number_array2)
+number_array3 = np.array(number_array3)
+#Listorna konverteras till arrays
+
+data = [[number_array], [number_array2], [number_array3]]
+df = pd.DataFrame(data, columns = ["Lottorader"])
+print(df)
+#Lägger till arrays till en databas
