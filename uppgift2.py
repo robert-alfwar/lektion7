fa0241a5fd58bcf0fe1a49b863d63c1de1ca6b28
[rbarrios1289] <[rbarrios1289@gmail.com]>
2020-09-07 15:56:04 +0200

    Uppfigt 2

---
+++ uppgift2.py
@@ -0,0 +1,7 @@
+#Uppgifter: 1. Gör ett program där du importerar hela modulerna math och cmath genom att skriva
+
+import math
+import cmath 
+#2. Skriv ut resultaten för de olika metoderna math.sin(1) och cmath.sin(1)
+print (math.sin(1))
+print (cmath.sin(1))
