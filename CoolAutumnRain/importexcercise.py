1e1da95a23307782a7e6dc0e03aa006630fa5d5c
alexanderlitholm <alexanderlitholm@gmail.com>
2020-09-07 16:48:08 +0200

    Added list comprehension and removed some variables.

--- CoolAutumnRain/importexcercise.py
+++ CoolAutumnRain/importexcercise.py
@@ -1,18 +1,14 @@
 import pandas as pd
 import numpy as np
 
-user_list = list()
 quarters = ['Q1', 'Q2', 'Q3', 'Q4']
 years = [2017, 2018, 2019]
 
 def input_value(year):
+    user_list = []
     for quarter in quarters:
         user_list.append(int(input(f'Fyll i resultat för {quarter} år {year}.\n: ')))
     return user_list
 
-a = np.array(input_value(2017)[0:4])
-b = np.array(input_value(2018)[4:8])
-c = np.array(input_value(2019)[8:12])
-
-df = pd.DataFrame([a, b, c], index=years, columns=quarters)
+df = pd.DataFrame([input_value(year) for year in years], index=years, columns=quarters)
 print(df)
