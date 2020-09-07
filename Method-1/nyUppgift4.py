7d990f8c680ece598eb7cdc34ab048495386b96e
Phillodelphia <42610104+Phillodelphia@users.noreply.github.com>
2020-09-07 15:58:43 +0200

    Update nyUppgift4.py
    I did mistake ;-;

--- Method-1/nyUppgift4.py
+++ Method-1/nyUppgift4.py
@@ -7,7 +7,7 @@ print(f"numbers inputed: {numbers}")
 arr = np.array(numbers) # konverterar listan numbers till numpy arrays
 splitArr = np.split(arr, 3) # splittar en numpy array till tre numpy arrays med fyra tal i varje
 
-years = pd.Series(pd.date_range("2000", periods=3, freq="Y"))
+years = pd.Series(pd.date_range("2017", periods=3, freq="Y"))
 
 df = pd.DataFrame(data=splitArr, index=[years.dt.year], columns=["Q1", "Q2", "Q3", "Q4"]) # kombinerar numpy arrayer till en dataframe
 print(df)
