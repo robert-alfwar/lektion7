a9a37fe9ce8f850d1d104d8cfbb060530f97f0cf
[rbarrios1289] <[rbarrios1289@gmail.com]>
2020-09-07 15:53:52 +0200

    Uppfigt 1

---
+++ uppgift1.py
@@ -0,0 +1,13 @@
+#Uppgifter:
+    #1. Kör kommandot "pip list" vad händer? varför?
+"""
+    Package         Version
+--------------- -------
+numpy           1.19.1
+pandas          1.1.1
+pip             20.2.2
+python-dateutil 2.8.1
+pytz            2020.1
+six             1.15.0"""
+
+#Lista över paket som jag har installerat.
