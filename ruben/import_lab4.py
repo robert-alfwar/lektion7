b82f9c69064da4af286ad70ed87d3ba24fa43526
rubenxyz <log@kamiko.xyz>
2020-09-07 19:06:09 +0200

    add year and quarter input

--- ruben/import_lab4.py
+++ ruben/import_lab4.py
@@ -1,14 +1,32 @@
 import pandas as pd
 import numpy as np
 
-# a, b, c, d, e, f, g, h, i, j, k, l = ,
-# list1 = [a, b, c, d, e, f]
-# list2 = [g, h, i, j, k, l]
-# np_array1 = np.asarray(list1)
-# np_array2 = np.asarray(list2)
-# dataframe = pd.DataFrame({'x':np_array1, 'y':np_array2})
-# dataframe.plot('x', 'y', kind='scatter')
+nums1 = []
+nums2 = []
+nums3 = []
+nums_list = [nums1, nums2, nums3]
+y = ["2017","2018","2019"]
+q = ["Q1", "Q2", "Q3", "Q4"]
+q_index = 0
+y_index = 0
 
-numpy_data = np.array([[12, 34, 45, 56, 67, 45]], [[77, 32, 96, 22, 132, 99]])
-df = pd.DataFrame(data=numpy_data)
-print(df)
+def business_cycle(nums_list, q, y, q_index, y_index):
+    print("\nProvide company revenue in millions of USD.")
+    for item in nums_list:
+        for element in range(4):
+            num = int(input(f"Input revenue for {q[q_index]} {y[y_index]}: "))
+            item.append(num)
+            q_index += 1
+        q_index = 0
+        y_index += 1
+    return nums_list
+
+nums_list = business_cycle(nums_list, q, y, q_index, y_index)
+
+np_arr1 = np.array(nums_list[0])
+np_arr2 = np.array(nums_list[1])
+np_arr3 = np.array(nums_list[2])
+
+dataset = pd.DataFrame({y[0] : np_arr1, y[1] : np_arr2, y[2] : np_arr3}, 
+    index=[q[0], q[1], q[2], q[3]])
+print(f'\n{dataset}')
