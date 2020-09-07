23adb35b1a246fcdc7dc2a8437656d48aee0e43a
alexanderlitholm <alexanderlitholm@gmail.com>
2020-09-07 16:26:25 +0200

    Changes made to importexercise.py

--- CoolAutumnRain/importexcercise.py
+++ CoolAutumnRain/importexcercise.py
@@ -1,9 +1,18 @@
 import pandas as pd
 import numpy as np
-user_list = input('''
-Mata in 4x3 tal (12 totalt, alla whitespace som delimiter): ''').split()
-a = np.array(user_list[0:4])
-b = np.array(user_list[4:8])
-c = np.array(user_list[8:12])
-df = pd.DataFrame([a, b, c], columns=['Col1', 'Col2', 'Col3', 'Col4'])
+
+user_list = list()
+quarters = ['Q1', 'Q2', 'Q3', 'Q4']
+years = [2017, 2018, 2019]
+
+def input_value(year):
+    for quarter in quarters:
+        user_list.append(int(input(f'Fyll i resultat för {quarter} år {year}.\n: ')))
+    return user_list
+
+a = np.array(input_value(2017)[0:4])
+b = np.array(input_value(2018)[4:8])
+c = np.array(input_value(2019)[8:12])
+
+df = pd.DataFrame([a, b, c], index=years, columns=quarters)
 print(df)
