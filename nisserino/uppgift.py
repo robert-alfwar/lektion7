d82ab7507d8a5f3923cf08163b96ffe091e56998
Zephyro Kemstedt <zkemstedt@gmail.com>
2020-09-07 14:24:53 +0200

    transposed the arrays, fixed the missing data

--- nisserino/uppgift.py
+++ nisserino/uppgift.py
@@ -3,12 +3,11 @@ import pandas as pd
 
 # note: missing user input
 sample_input = [1, 3, 5, 6, 8, 12, 77, 32, 99, 13, 42, 156]
-array1 = np.array(sample_input[0:3])
-array2 = np.array(sample_input[4:7])
-array3 = np.array(sample_input[8:11])
-# note: missing 1 row
+array1 = np.array(sample_input[0:4])
+array2 = np.array(sample_input[4:8])
+array3 = np.array(sample_input[8:12])
 
 df = pd.DataFrame(data=[array1, array2, array3],
-                  columns=['2017', '2018', '2019'],
-                  index=['Q1', 'Q2', 'Q3'])
+                  index=['2017', '2018', '2019'],
+                  columns=['Q1', 'Q2', 'Q3', 'Q4'])
 print(df)
