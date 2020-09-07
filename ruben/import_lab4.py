2ab75c3e71d8a5bfdf9e786e2c72fa9623e0b37b
rubenxyz <log@kamiko.xyz>
2020-09-07 15:16:44 +0200

    Uppdateringar 15:16

--- ruben/import_lab4.py
+++ ruben/import_lab4.py
@@ -1,10 +1,14 @@
 import pandas as pd
 import numpy as np
 
-a, b, c, d, e, f, g, h, i, j, k, l = 12, 34, 45, 56, 67, 45, 99, 77, 32, 96, 22, 132
-list1 = [a, b, c, d, e, f]
-list2 = [g, h, i, j, k, l]
-np_array1 = np.asarray(list1)
-np_array2 = np.asarray(list2)
-dataframe = pd.DataFrame({'x':np_array1, 'y':np_array2})
-dataframe.plot('x', 'y', kind='scatter')
+# a, b, c, d, e, f, g, h, i, j, k, l = ,
+# list1 = [a, b, c, d, e, f]
+# list2 = [g, h, i, j, k, l]
+# np_array1 = np.asarray(list1)
+# np_array2 = np.asarray(list2)
+# dataframe = pd.DataFrame({'x':np_array1, 'y':np_array2})
+# dataframe.plot('x', 'y', kind='scatter')
+
+numpy_data = np.array([[12, 34, 45, 56, 67, 45]], [[77, 32, 96, 22, 132, 99]])
+df = pd.DataFrame(data=numpy_data)
+print(df)
