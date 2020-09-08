6b1202d267d4a1918ea724c923d1def8afd8450a
YesblobbenNA <70510729+YesblobbenNA@users.noreply.github.com>
2020-09-08 11:51:35 +0200

    Daniel

--- YesblobbenNA/Danielnumpypandas.py
+++ YesblobbenNA/Danielnumpypandas.py
@@ -29,7 +29,7 @@ number_array2 = np.array(number_array2)
 number_array3 = np.array(number_array3)
 #Listorna konverteras till arrays
 
-data = [number_array, number_array2, number_array3]
-df = pd.DataFrame(data, columns = ["Lotto 1", "Lotto 2", "Lotto 3", "Lotto 4"])
+data = {"Lotto 1" : number_array, "Lotto 2" : number_array2, "Lotto 3" : number_array3}
+df = pd.DataFrame(data, index = ["Rad 1", "Rad 2", "Rad 3", "Rad 4"])
 print(df)
 #Lägger till arrays till en databas
